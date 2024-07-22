f73e3a257b00a825f7cc9b87a3da91b5535a65fd
bperalta <bperoliva@gmail.com>
2024-07-22 16:05:19 -0400

    refactor: update usePersonData hook to handle data filtering and storage

--- react-nextjs/hooks/usePersonData.js
+++ react-nextjs/hooks/usePersonData.js
@@ -2,36 +2,69 @@ import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const usePersonData = () => {
-  // Estados para manejar los datos y el filtro
+  // Hook de Estados para manejar los datos y el filtro
   const [data, setData] = useState([]);
   const [filter, setFilter] = useState("todos");
 
   // Función para guardar datos en localStorage
   const saveToLocalStorage = (data) => {
-    localStorage.setItem("data", JSON.stringify(data)); 
+    localStorage.setItem("data", JSON.stringify(data));
   };
 
-  // Efecto para inicializar datos desde localStorage o establecer datos iniciales
+  // Hook de Efecto para inicializar datos desde localStorage o establecer datos iniciales, al usar [] solo se ejecutaría una vez
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem("data"));
-    const initialData = [ 
-      { id: 1, apellidos: "Pérez", nombres: "José", genero: "Masculino", estadoCivil: "Soltero(a)", sueldo: "2500000" },
-      { id: 2, apellidos: "Gómez", nombres: "María", genero: "Femenino", estadoCivil: "Casado(a)", sueldo: "2500000" },
-      { id: 3, apellidos: "Mendoza", nombres: "Roberto", genero: "Masculino", estadoCivil: "Casado(a)", sueldo: "2500000" },
-      { id: 4, apellidos: "López", nombres: "Catalina", genero: "Femenino", estadoCivil: "Soltero(a)", sueldo: "2500000" }
+    const initialData = [
+      {
+        id: 1,
+        apellidos: "Pérez",
+        nombres: "José",
+        genero: "Masculino",
+        estadoCivil: "Soltero(a)",
+        sueldo: "2500000",
+      },
+      {
+        id: 2,
+        apellidos: "Gómez",
+        nombres: "María",
+        genero: "Femenino",
+        estadoCivil: "Casado(a)",
+        sueldo: "2500000",
+      },
+      {
+        id: 3,
+        apellidos: "Mendoza",
+        nombres: "Roberto",
+        genero: "Masculino",
+        estadoCivil: "Casado(a)",
+        sueldo: "2500000",
+      },
+      {
+        id: 4,
+        apellidos: "López",
+        nombres: "Catalina",
+        genero: "Femenino",
+        estadoCivil: "Soltero(a)",
+        sueldo: "2500000",
+      },
     ];
-    !storedData ? (setData(initialData), saveToLocalStorage(initialData)) : setData(storedData);
+    !storedData
+      ? (setData(initialData), saveToLocalStorage(initialData))
+      : setData(storedData);
   }, []);
 
-  // Efecto para filtrar datos según el género
+  // Efecto para filtrar datos según el género, se ejecuta cuando cambia el valor de filter
   useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem("data")) || []; 
+    const storedData = JSON.parse(localStorage.getItem("data")) || [];
     refreshData(storedData);
   }, [filter]);
 
   // Función para refrescar los datos según el filtro
   const refreshData = (data) => {
-    const filteredData = filter === "todos" ? data : data.filter((person) => person.genero === filter);
+    const filteredData =
+      filter === "todos"
+        ? data
+        : data.filter((person) => person.genero === filter);
     setData(filteredData);
   };
 
@@ -48,7 +81,7 @@ const usePersonData = () => {
   const updatePerson = (updatePerson) => {
     const storedData = JSON.parse(localStorage.getItem("data")) || [];
     const updatedData = storedData.map((person) =>
-      person.id === updatePerson.id ? updatePerson : person 
+      person.id === updatePerson.id ? updatePerson : person
     );
     setData(updatedData);
     saveToLocalStorage(updatedData);
@@ -57,7 +90,9 @@ const usePersonData = () => {
 
   // Función para eliminar una persona
   const deletePerson = (id) => {
-    if (window.confirm("¿Estás seguro de que quieres eliminar este registro?")) {
+    if (
+      window.confirm("¿Estás seguro de que quieres eliminar este registro?")
+    ) {
       const updatedData = data.filter((person) => person.id !== id);
       setData(updatedData);
       saveToLocalStorage(updatedData);
@@ -70,7 +105,7 @@ const usePersonData = () => {
     updatePerson,
     deletePerson,
     filter,
-    setFilter
+    setFilter,
   };
 };
 
